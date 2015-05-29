5e73db7657e683a25d896f4b8b1e78ef3931a450
milesdevis <dev_raj82@yahoo.com>
2015-05-30 01:20:32 +0530

    PDF Inspection Fields

---
+++ GenerateReport/GenerateReport/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@     Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GenerateReport._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+  
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h3>Export To PDF</h3>
+    <div>
+        <asp:Button ID="Button1" runat="server" Text="Generate Report" OnClick="Button1_Click" />
+       
+    </div>
+</asp:Content>
---
+++ GenerateReport/GenerateReport/Default.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace GenerateReport
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string fileName = Guid.NewGuid() + ".pdf";
+            string filePath = Path.Combine(Server.MapPath("~/PDFFiles"), fileName);
+
+            Document doc = new Document(PageSize.A4, 2, 2, 2, 2);
+            Paragraph p = new Paragraph("Export Database data to PDF file");
+
+            try
+            {
+                PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
+                PdfPTable pdfTab = new PdfPTable(146);
+                pdfTab.HorizontalAlignment = 1;
+                pdfTab.SpacingBefore = 20f;
+
+                List<Table> data = new List<Table>();
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    data = dc.Tables.ToList();
+                }
+
+                foreach (var item in data)
+                {
+                    pdfTab.AddCell(item.Device_ID.ToString());
+                    //ADD Everthing else as well
+
+                }
+
+                doc.Open();
+                doc.Add(p);
+                doc.Add(pdfTab);
+                doc.Close();
+
+                byte[] content = File.ReadAllBytes(filePath);
+                HttpContext context = HttpContext.Current;
+
+                context.Response.BinaryWrite(content);
+                context.Response.ContentType = "application/pdf";
+                context.Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+                context.Response.End();
+
+
+            }
+
+            catch
+            {
+                throw;
+
+            }
+
+            finally
+            {
+                doc.Close();
+            }
+        }
+    }
+}
